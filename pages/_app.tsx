2babb2c928cb0524fe4282f22ae666b635a27299
Celestin Niyindagiriye <nicelestin08@gmail.com>
2022-12-31 23:10:28 +0200

    add loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
+
+const ProgressBar = dynamic(
+  () => import('@/components/shared/ProgessBar'),
+  {
+    ssr: false,
+  },
+);
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <ProgressBar />
+    </>
+  );
 };
 
 export default App;
